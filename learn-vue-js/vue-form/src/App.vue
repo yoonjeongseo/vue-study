84936fbe09e0ad645a7fb970c0fcb3b072367239
seoyoonjeong <yoonnjeong@easymedia.net>
2022-11-16 19:33:56 +0900

    221116

---
+++ learn-vue-js/vue-form/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <form v-on:submit.prevent="SubmitForm">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data: function() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    SubmitForm: function() {
+      // e.preventDefault();
+      console.log(this.username, this.password);
+      var url = 'https://jsonplaceholder.typicode.com/users';
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+      //axios는 데이터를 주고 받기 위한 라이브러리 post는 데이터를 생성하거나 조작헐 수 있게 하는 메서드
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
